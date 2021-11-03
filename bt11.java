417ebb5419e7d0aed84da63d6b2573900da36a5b
Nguyễn Quang Hà <81666456+thisisneon@users.noreply.github.com>
2021-11-03 15:50:16 +0700

    Create bt11.java

---
+++ bt11.java
@@ -0,0 +1,31 @@
+package Ham;
+import java.util.Scanner;
+import java.util.Random;
+/**
+ *
+ * @author USER
+ */
+public class b11 {
+     public static void main(String[] args) {
+        int[] a = new int[5];
+        getRandValues(a);
+    }
+   
+    public static void getRandValues(int[] a) {
+        System.out.println("Enter values: ");
+        int len = a.length;
+        for (int i = 0; i < len; i++) {
+            Random rand = new Random();
+            a[i] = rand.nextInt(100)+1;
+        }
+        System.out.println("Xuoi Array a: ");
+            for(int i = 0; i < len; i++) {               
+            System.out.print(a[i] + " ");
+        }
+        System.out.println("");
+        System.out.println("Nguoc Array a: ");
+        for (int i = len - 1; i >= 0; i--) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}

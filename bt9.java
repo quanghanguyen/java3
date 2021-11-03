ee2eb8077038fd078aa3c7219472f07a5b82981b
Nguyễn Quang Hà <81666456+thisisneon@users.noreply.github.com>
2021-11-03 15:49:15 +0700

    Create bt9.java

---
+++ bt9.java
@@ -0,0 +1,35 @@
+package Ham;
+
+import java.util.Random;
+/**
+ *
+ * @author USER
+ */
+public class b9 {
+     public static void main(String[] args) {
+        int[] a = new int[5];
+        b9.getRandValues(a);
+        float avg = b9.Average(a);
+        System.out.print("Average of above Array: " + avg);
+    }
+   
+    public static void getRandValues(int[] a) {
+        System.out.println("Enter values: ");
+        for (int i = 0; i < a.length; i++) {
+            Random rand = new Random();
+            a[i] = rand.nextInt(100)+1;
+        }
+        System.out.println("Array: ");
+        for(int i = 0; i < a.length; i++)               
+        System.out.println(a[i]);
+    }
+    
+    public static float Average(int[] a) {
+        float avg = 0;
+        for (int i = 0; i < a.length; i++) {
+            avg += a[i];
+        }
+        avg /= (float)(a.length);
+        return avg;
+    }
+}
